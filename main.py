10e537feec4b8a7f6ab85f3b1decde779f581c13
priyansu dash <108187409+priyansudash03@users.noreply.github.com>
2024-10-25 23:52:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from constants import MAJOR_COLORS, MINOR_COLORS
+from color_utils import get_color_from_pair_number, get_pair_number_from_color
 from test_color_code import test_number_to_pair, test_pair_to_number
 
 if __name__ == '__main__':
-    test_number_to_pair(4, 'White', 'Brown')
-    test_number_to_pair(5, 'White', 'Slate')
-    test_pair_to_number('Black', 'Orange', 12)
-    test_pair_to_number('Violet', 'Slate', 25)
-    test_pair_to_number('Red', 'Orange', 7)
+  test_number_to_pair(4, 'White', 'Brown')
+  test_number_to_pair(5, 'White', 'Slate')
+  test_pair_to_number('Black', 'Orange', 12)
+  test_pair_to_number('Violet', 'Slate', 25)
+  test_pair_to_number('Red', 'Orange', 7)
+  print('Done :)')
 
-    print("All tests passed!")
