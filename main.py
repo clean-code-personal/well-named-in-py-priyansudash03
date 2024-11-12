44879667fd4fd30c7b9e40bbdf3641046d4b704c
priyansu dash <108187409+priyansudash03@users.noreply.github.com>
2024-11-12 11:06:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from constants import MAJOR_COLORS, MINOR_COLORS
 from color_utils import get_color_from_pair_number, get_pair_number_from_color
 from test_color_code import test_number_to_pair, test_pair_to_number
+from print_color_coding import print_color_coding_reference
 
 if __name__ == '__main__':
   test_number_to_pair(4, 'White', 'Brown')
@@ -10,3 +11,4 @@ if __name__ == '__main__':
   test_pair_to_number('Red', 'Orange', 7)
   print('Done :)')
 
+print_color_coding_reference()
